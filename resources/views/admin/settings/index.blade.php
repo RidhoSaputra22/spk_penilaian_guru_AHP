6171557cd63c73dcce55658dc644f6131a2d36a7
ridho <ridho@gmail.com>
2026-02-26 10:13:59 +0800

    -

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -44,7 +44,7 @@
                         </svg>
                         Kelompok Guru
                     </a></li>
-                <li><a href="{{ route('admin.settings.index', ['tab' => 'ahp']) }}"
+                <!-- <li><a href="{{ route('admin.settings.index', ['tab' => 'ahp']) }}"
                         class="{{ $tab === 'ahp' ? 'active' : '' }}">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor">
@@ -52,7 +52,7 @@
                                 d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
                         </svg>
                         AHP
-                    </a></li>
+                    </a></li> -->
                 <!-- <li class="menu-title">Sistem</li>
                 <li><a href="{{ route('admin.settings.index', ['tab' => 'email']) }}"
                         class="{{ $tab === 'email' ? 'active' : '' }}">
